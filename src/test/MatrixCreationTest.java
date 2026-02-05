1575b2e296a1779bb685522d4b859314128ecf04
Arnav <arniemerani@gmail.com>
2026-02-05 11:35:16 -0500

    complete preliminary test suite for Matrix constructors and creation methods

--- src/test/MatrixCreationTest.java
+++ src/test/MatrixCreationTest.java
@@ -93,17 +93,82 @@ public class MatrixCreationTest {
     @DisplayName("Scalar factory creates 1x1 matrix with the given value")
     void scalarFactoryCreatesUnitMatrix() {
 
+        Matrix scalarMatrix = Matrix.createScalarMatrix(3, 5);
+        for (int i = 0; i < scalarMatrix.getRows(); i++) {
+            for (int j = 0; j < scalarMatrix.getColumns(); j++) {
+                if (i == j) {
+                    assertEquals(5, scalarMatrix.getEntry(i,j), TOLERANCE);
+
+                } else {
+                    assertEquals(0, scalarMatrix.getEntry(i,j), TOLERANCE);
+
+                }
+            }
+        }
+
+        Matrix identityMatrix = Matrix.createIdentityMatrix(6);
+        for (int i = 0; i < identityMatrix.getRows(); i++) {
+            for (int j = 0; j < identityMatrix.getColumns(); j++) {
+                if (i == j) {
+                    assertEquals(1, identityMatrix.getEntry(i,j), TOLERANCE);
+
+                } else {
+                    assertEquals(0, identityMatrix.getEntry(i,j), TOLERANCE);
+
+                }
+            }
+        }
+
     }
 
     @Test
     @DisplayName("ofRows method creates matrix correctly.")
     void ofRowsFactoryMethodCreatesCorrectMatrix() {
+        double[] row1 = {1.5, -2, 7.25};
+        double[] row2 = {0, 3.14, -999.1};
+
+        double[][] rows = {row1, row2}; //To be used for iterating and testing
+
+        Matrix madeOfRows = Matrix.ofRows(row1, row2);
+
+        assertEquals(2, madeOfRows.getRows());
+        assertEquals(3, madeOfRows.getColumns());
+
+        for (int i = 0; i < madeOfRows.getRows(); i++) {
+            for (int j = 0; j < madeOfRows.getColumns(); j++) {
+                assertEquals(rows[i][j], madeOfRows.getEntry(i,j), TOLERANCE);
+            }
+        }
+
+        row1[0] = 9999;
+        assertEquals(1.5, madeOfRows.getEntry(0,0), TOLERANCE);
 
     }
 
     @Test
     @DisplayName("ofColumns method creates matrix correctly.")
     void ofColumnsFactoryMethodCreatesCorrectMatrix() {
+        double[] col1 = {1.5, 0};
+        double[] col2 = {-2, 3.14};
+        double[] col3 = {7.25, -999.1};
 
+        double[] r1 = {1.5, -2, 7.25};
+        double[] r2 = {0, 3.14, -999.1};
+
+        double[][] cols = {r1, r2}; //To be used for iterating and testing - represents what the matrix should look like
+
+        Matrix madeOfCols = Matrix.ofColumns(col1, col2, col3);
+
+        assertEquals(2, madeOfCols.getRows());
+        assertEquals(3, madeOfCols.getColumns());
+
+        for (int i = 0; i < madeOfCols.getRows(); i++) {
+            for (int j = 0; j < madeOfCols.getColumns(); j++) {
+                assertEquals(cols[i][j], madeOfCols.getEntry(i,j), TOLERANCE);
+            }
+        }
+
+        col1[1] = 7253;
+        assertEquals(0, madeOfCols.getEntry(1,0), TOLERANCE);
     }
 }
